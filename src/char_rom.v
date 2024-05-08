98f71ae790e914bc65ff8986ebec14e510d703c5
Ciro Cattuto <ciro.cattuto@gmail.com>
2024-05-08 11:08:29 +0200

    back to wire port in ROM

--- src/char_rom.v
+++ src/char_rom.v
@@ -5,7 +5,7 @@ module char_rom #(
     parameter ADDR_MAX = 126
 )(
     input wire [ADDR_WIDTH-1:0] address,
-    output reg [DATA_WIDTH-1:0] data
+    output wire [DATA_WIDTH-1:0] data
 );
 
 reg [DATA_WIDTH-1:0] mem [0:ADDR_MAX-ADDR_MIN+2];
@@ -14,12 +14,6 @@ initial begin
     $readmemb("font.bin", mem);  // load char bitmaps from file
 end
 
-always @(address) begin
-    if (address >= ADDR_MIN && address <= ADDR_MAX) begin
-        data = mem[address-ADDR_MIN];
-    end else begin
-        data = mem[ADDR_MAX-ADDR_MIN+1];
-    end
-end
+assign data = (address >= ADDR_MIN && address <= ADDR_MAX) ? mem[address-ADDR_MIN] : mem[ADDR_MAX-ADDR_MIN+1];
 
 endmodule
