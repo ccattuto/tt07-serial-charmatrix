b8e1ee181cb4eafd8eee098e1ea74f116e20f809
Ciro Cattuto <ciro.cattuto@gmail.com>
2024-05-11 20:16:45 +0200

    initial tests

--- test/test.py
+++ test/test.py
@@ -3,38 +3,102 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+import cocotb.result
+from cocotb.triggers import Timer, Edge, with_timeout
+import cocotb.simulator as simulator
+import random
 
 
-@cocotb.test()
+@cocotb.test(timeout_time=25, timeout_unit='ms')
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    # Set the clock period to 50 ns (20 MHz)
+    clock = Clock(dut.clk, 50, units="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset
+    # signals
+    uart_rx = dut.ui_in[3]
+    led = dut.uo_out[0]
+
+    # reset
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
+    uart_rx.value = 1 # keep RX high
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await Timer(1, units="us")
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    assert led.value == 0
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    c1 = await get_char(dut, led)
+    c2 = await get_char(dut, led)
+    c3 = await get_char(dut, led)
+    c4 = await get_char(dut, led)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await Timer(1.2, units="ms")
+    await do_tx(uart_rx, 9600, ord('A'))
+    #await Edge(dut.user_project.uart_rx_ready)
+    #assert dut.user_project.uart_rx_ready.value == 1
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    c1 = await get_char(dut, led)
+    c2 = await get_char(dut, led)
+    c3 = await get_char(dut, led)
+    c4 = await get_char(dut, led)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+
+async def do_tx(uart_rx, baud, data):
+    # prepare random test data
+    TEST_BITS_LSB = [(data >> s) & 1 for s in range(8)]
+
+    # send start bit (0), 8 data bits, stop bit (1)
+    for tx_bit in [0] + TEST_BITS_LSB + [1]:
+        uart_rx.value = tx_bit
+        await Timer(int(1.0 / baud * 1e12), units="ps")
+
+async def get_char(dut, led):
+    cseq = []
+    for count in range(35):
+        bitseq = await get_24bits(dut, led)
+        dut._log.info(f"{count}: {bitseq}")
+        cseq.append( 0 if sum(bitseq) == 0 else 1 )
+
+    for i in range(7):
+        linestring = "".join(["O" if x==1 else "." for x in cseq[i*5:(i+1)*5]])
+        dut._log.info(linestring)
+
+    return "".join([str(x) for x in cseq])
+        
+async def get_24bits(dut, led):
+    bitseq = []
+
+    for i in range(24):
+        while led.value == 0:
+            await Edge(dut.uo_out)
+        th1, tl1 = simulator.get_sim_time()
+
+        while led.value == 1:
+            await Edge(dut.uo_out)
+        th2, tl2 = simulator.get_sim_time()
+
+        pulse_us = (tl2-tl1) / 1000
+        assert(pulse_us > 300)
+        assert(pulse_us < 900)
+        #dut._log.info(pulse_us)
+
+        bitseq.append( 1 if (pulse_us > 625) else 0 )
+
+    return bitseq
+
+async def ledreset(dut, led):
+    reset_detected = 0
+    while not reset_detected:
+        while led.value == 1:
+            await Edge(dut.uo_out)
+        try:
+            await with_timeout(Edge(dut.uo_out), 50, 'us')
+        except cocotb.result.SimTimeoutError:
+            reset_detected = 1
+    
